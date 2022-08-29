862a5cf0125506ebf4ca953e3e8f4d848fde96f9
NekoMio <hui@nekomio.com>
2022-08-29 22:50:05 +0800

    UI 完善

--- src/App.vue
+++ src/App.vue
@@ -5,88 +5,120 @@
       background: linear-gradient(156.78deg, #5367c9 29.61%, #b691c1 85.65%);
     "
   >
-    <canvas
-      class="w-2/3 h-2/3 bottom-[16.666667%] left-[8%] fixed z-50"
-      id="three"
-    ></canvas>
-    <div class="w-screen h-screen left-0 top-0 z-10 fixed">
-      <div class="left-4 top-6 w-40 h-32 flex flex-col items-center">
+    <Transition name="fade">
+      <canvas
+        v-if="!showEnd"
+        class="w-2/3 h-2/3 bottom-[16.666667%] left-[15%] fixed z-50"
+        id="three"
+      ></canvas
+    ></Transition>
+    <Transition name="fade">
+      <div
+        v-if="showEnd"
+        class="w-screen h-screen left-0 top-0 z-10 fixed flex flex-row items-center justify-center"
+      >
         <div
-          class="m-0 text-xs not-italic font-serif font-normal leading-4 text-white"
+          class="xl:w-1/3 w-2/3 h-1/2 rounded-lg shadow-2xl shadow-slate-800"
+          style="background-color: rgba(16, 15, 70, 0.5)"
+        >
+          结果
+        </div>
+      </div>
+    </Transition>
+    <div class="w-screen h-screen left-0 top-0 z-10 fixed">
+      <div class="left-16 top-10 w-48 h-32 flex flex-col items-center">
+        <div
+          class="m-0 text-sm not-italic font-serif font-normal leading-5 text-white"
         >
           亚运会体育图标动作模仿游戏
         </div>
         <img class="mt-2 w-28 h-28" src="./assets/img/logo.svg" alt="logo" />
       </div>
-      <div
-        class="right-4 top-6 w-56 h-auto fixed flex flex-col rounded-lg shadow-2xl shadow-slate-800"
-        style="background-color: rgba(16, 15, 70, 0.5)"
-      >
+      <Transition name="fade">
         <div
-          v-if="!started"
-          class="w-32 ml-3 mt-10 text-xl leading-6 font-normal font-serif text-white animate-pulse"
+          v-if="!showEnd"
+          class="xl:left-[70%] right-8 top-6 w-56 h-auto fixed flex flex-col rounded-lg shadow-2xl shadow-slate-800"
+          style="background-color: rgba(16, 15, 70, 0.5)"
         >
-          模仿这个动作开始游戏吧！
-        </div>
-        <template v-else>
           <div
-            class="w-32 ml-3 mt-4 text-xl leading-6 font-normal font-serif text-white"
+            v-if="!started"
+            class="w-32 ml-3 mt-10 text-xl leading-6 font-normal font-serif text-white animate-pulse"
           >
-            当前动作
+            模仿这个动作开始游戏吧！
           </div>
-          <div
-            class="w-32 ml-3 mt-4 text-xl leading-6 font-normal font-serif text-white"
-          >
-            <span
-              class="ml-1 icon-before icon-solid icon-clock-rotate-left"
-            ></span>
-            <span class="ml-3">{{ currentTime }} s</span>
-          </div>
-        </template>
-        <div class="w-3/4 mx-3 my-4 bg-white h-0.5"></div>
-        <div
-          class="w-32 ml-3 text-xl leading-6 font-normal font-serif text-white"
-        >
-          {{ poseNameCN }}
-        </div>
-        <div
-          class="w-32 ml-3 text-xl leading-6 font-normal font-serif text-white"
-        >
-          {{ poseNameEN }}
-        </div>
-        <img class="w-52 mx-auto mt-3 mb-5" :src="posePic" />
-      </div>
-      <template v-if="started">
-        <div
-          class="h-1/2 w-2.5 left-9 rounded-full top-8"
-          style="
-            background: linear-gradient(
-              180deg,
-              rgb(0, 255, 0, 255) 0%,
-              rgb(255, 0, 0, 255) 100%
-            );
-          "
-        >
-          <div
-            class="w-5 h-5 top-32 -left-[0.3125rem] absolute rounded-full bg-white blur-[2px]"
-          ></div>
-          <!-- <div class="h-full w-full rounded-full" style="background: linear-gradient(180deg, #E48225 0%, rgba(254, 223, 77, 0) 100%)"> -->
-          <div
-            class="w-full absolute bottom-0 block rounded-full"
-            :style="{ height: percentage + '%' }"
-          >
+          <div v-if="started">
             <div
-              class="rounded-full w-11 h-11 left-6 border-[3px] -top-5 mb-4 border-solid border-white text-xl text-white font-mono font-normal flex items-center justify-around flex-col"
+              class="w-32 ml-3 mt-4 text-xl leading-6 font-normal font-serif text-white"
             >
-              {{ percentage }}
+              当前动作
             </div>
             <div
-              class="w-6 h-6 -top-2.5 -left-[0.4375rem] absolute rounded-full bg-yellow-200 blur-[2px]"
-            ></div>
+              class="w-32 ml-3 mt-4 text-xl leading-6 font-normal font-serif text-white"
+            >
+              <span
+                class="ml-1 icon-before icon-solid icon-clock-rotate-left"
+              ></span>
+              <span class="ml-3"
+                >{{
+                  this.PrefixInteger(Math.floor(currentTime / 60)) +
+                  ":" +
+                  this.PrefixInteger(currentTime % 60)
+                }}
+                s</span
+              >
+            </div>
           </div>
-          <!-- </div> -->
+          <div class="w-3/4 mx-3 my-4 bg-white h-0.5"></div>
+          <div
+            class="w-32 ml-3 text-xl leading-6 font-normal font-serif text-white"
+          >
+            {{ poseNameCN }}
+          </div>
+          <div
+            class="w-32 ml-3 text-xl leading-6 font-normal font-serif text-white"
+          >
+            {{ poseNameEN }}
+          </div>
+          <img
+            v-if="currentTime < 20"
+            class="w-52 mx-auto mt-3 mb-5"
+            :src="posePic"
+            alt="静态图"
+          />
+          <img
+            v-else
+            class="w-52 mx-auto mt-3 mb-5"
+            :src="posePic"
+            alt="动态图"
+          />
         </div>
-      </template>
+      </Transition>
+      <Transition name="fade">
+        <template v-if="started && !showEnd">
+          <div
+            class="h-1/2 w-2.5 xl:left-96 left-32 top-52 fixed rounded-full bar"
+          >
+            <div
+              class="w-5 h-5 -top-2.5 -left-[0.3125rem] absolute rounded-full bg-white blur-[2px]"
+            ></div>
+            <div
+              class="w-full absolute bottom-0 block rounded-full"
+              :style="{ height: percentage + '%' }"
+            >
+              <div
+                class="rounded-full w-11 h-11 left-6 border-[3px] -top-5 mb-4 border-solid border-white text-xl text-white font-mono font-normal flex items-center justify-around flex-col"
+              >
+                {{ percentage }}
+              </div>
+              <div
+                class="w-6 h-6 -top-2.5 -left-[0.4375rem] absolute rounded-full blur-[2px]"
+                :style="{ backgroundColor: nowColor }"
+              ></div>
+            </div>
+            <!-- </div> -->
+          </div>
+        </template>
+      </Transition>
     </div>
     <img
       class="bottom-0 w-screen h-auto z-0 fixed"
@@ -100,22 +132,53 @@
 import {
   Scene,
   WebGLRenderer,
-  AxesHelper,
   PerspectiveCamera,
   Vector3,
   Vector2,
-  Euler,
   Quaternion,
   DirectionalLight,
   HemisphereLight,
   SkeletonHelper,
+  Mesh,
+  CircleGeometry,
+  MeshMatcapMaterial,
 } from "three";
-import { inject } from "vue";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 let bones = [1, 2, 4, 5, 7, 8, 11, 12, 14, 15]; // 親ボーン
 let child_bones = [2, 3, 5, 6, 8, 10, 12, 13, 15, 16];
+let englishToChinese = {
+  Archery: "射箭",
+  Shooting: "射击",
+  Boxing: "拳击",
+  Wushu: "武术",
+  Athletics: "田径",
+  "Canoe Sprint": "皮划艇静水",
+  Equestrian: "马术",
+  Fencing: "击剑",
+  Karate: "空手道",
+  "Roller Skating": "轮滑",
+  Skateboarding: "滑板",
+  "Sport Climbing": "攀岩",
+  Taekwondo: "跆拳道",
+  Triathlon: "铁人三项",
+  Weightlifting: "举重",
+  Basketball: "五人制篮球",
+  Badminton: "羽毛球",
+  Baseball: "棒球",
+  Cricket: "板球",
+  Football: "足球",
+  Golf: "高尔夫",
+  Handball: "手球",
+  Hockey: "曲棍球",
+  "Rugby Sevens": "七人制橄榄球",
+  "Soft Tennis": "软式网球",
+  Softball: "垒球",
+  Squash: "壁球",
+  "Table Tennis": "乒乓球",
+  Volleyball: "排球",
+};
 let init_inv = [];
 let init_rot = [];
 let scale_ratio = 0.003;
@@ -129,15 +192,34 @@ export default {
       poseNameCN: "跆拳道",
       poseNameEN: "Taekwondo",
       posePic: "/pic/cut/pose-09-crop.png",
-      percentage: 40,
+      percentage: 10,
       started: false,
-      currentTime: "00:14",
+      currentTime: 14,
       preData: {},
+      colorArray: [],
+      nowColor: "rgb(1,3,4)",
+      showEnd: false,
+      rankList: [],
+      totalTime: 0,
       // socket = inject("socket"),
     };
   },
   mounted() {
     this.initThree();
+    this.colorArray = this.colorArrToString(
+      this.colorGradient4(
+        [255, 50, 50],
+        [255, 153, 49],
+        [255, 236, 98],
+        [166, 255, 164],
+        3056,
+        6806,
+        10000
+      )
+    );
+    // console.log(this.colorArray);
+    this.nowColor = this.colorArray[this.percentage * 100];
+    // console.log(this.nowColor);
     this.$socket.on("connect", () => {
       console.log("connect");
       this.$socket.emit("connection", "connected");
@@ -155,28 +237,71 @@ export default {
           this.currentTime = 0;
         }
       }
-      if (data.started === false) {
-        if (this.started === true) {
-          this.showEndPage();
-        }
+      this.showEnd = data.end;
+      if (this.showEnd === true) {
+        setTimeout(() => {
+          this.showEnd = false;
+          this.totalTime = 0;
+        }, 10000);
       }
-
+      this.rankList = data.ranklist;
       this.started = data.started;
       this.percentage = data.percentage;
-      this.poseNameCN = data.poseID;
+      this.nowColor = this.colorArray[this.percentage * 100];
+      this.poseNameCN = englishToChinese[data.poseID];
       this.poseNameEN = data.poseID;
       this.posePic = this.getPicUrl(data.poseID);
       this.updatePos(data["3dpose"]);
     });
-    // setInterval(() => {
-    //   this.percentage += 1;
-    //   // this.currentTime = this.getTime();
-    // }, 200);
+    setInterval(() => {
+      this.currentTime++;
+      if (this.currentTime > 20) {
+        this.started = true;
+      }
+    }, 1000);
   },
   methods: {
+    colorGradient2(colorStart, colorEnd, steps) {
+      let colorArr = [];
+      // console.log(colorStart, colorEnd, steps);
+      for (let i = 0; i < steps; i++) {
+        colorArr.push([
+          Math.floor(
+            colorStart[0] + ((colorEnd[0] - colorStart[0]) * i) / steps
+          ),
+          Math.floor(
+            colorStart[1] + ((colorEnd[1] - colorStart[1]) * i) / steps
+          ),
+          Math.floor(
+            colorStart[2] + ((colorEnd[2] - colorStart[2]) * i) / steps
+          ),
+        ]);
+      }
+      // console.log(colorArr);
+      return colorArr;
+    },
+    colorGradient4(color1, color2, color3, color4, step1, step2, step3) {
+      let colorArr = [];
+      colorArr = colorArr.concat(this.colorGradient2(color1, color2, step1));
+      colorArr = colorArr.concat(
+        this.colorGradient2(color2, color3, step2 - step1)
+      );
+      colorArr = colorArr.concat(
+        this.colorGradient2(color3, color4, step3 - step2 + 1)
+      );
+      return colorArr;
+    },
+    colorArrToString(colorArr) {
+      return colorArr.map((item) => {
+        return `rgb(${item[0]}, ${item[1]}, ${item[2]})`;
+      });
+    },
     getPicUrl(poseNameEN) {
       return `/pic/cut/${poseNameEN}.png`;
     }, // 获取图片url
+    PrefixInteger(num) {
+      return (Array(2).join("0") + num).slice(-2);
+    },
     initThree() {
       scene = new Scene();
       // scene.background = new THREE.Color("0x00000000");
@@ -279,13 +404,13 @@ export default {
         bone_t[15] = anim.GetBoneTransform(HumanBodyBones.RightLowerArm);
         bone_t[16] = anim.GetBoneTransform(HumanBodyBones.RightHand);
       */
-      // let floorGeometry = new THREE.PlaneGeometry(3000, 3000);
-      // let floorMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-      // let floor = new THREE.Mesh(floorGeometry, floorMaterial);
-      // floor.rotation.x = -0.5 * Math.PI;
-      // floor.receiveShadow = true;
-      // floor.position.y = -0.001;
-      // scene.add(floor);
+      let floorGeometry = new CircleGeometry(1, 128);
+      let floorMaterial = new MeshMatcapMaterial({ color: 0xffffff });
+      let floor = new Mesh(floorGeometry, floorMaterial);
+      floor.rotation.x = -0.5 * Math.PI;
+      floor.receiveShadow = true;
+      floor.position.y = -0.8;
+      scene.add(floor);
 
       const dirLight = new DirectionalLight(0xffffff, 0.6);
       //光源等位置
@@ -296,7 +421,9 @@ export default {
       scene.add(dirLight);
       const hemLight = new HemisphereLight(0xffffff, 0xffffff, 0.6);
       hemLight.position.set(0, 48, 0);
-      scene.add(hemLight);
+      const xHemLight = new HemisphereLight(0xffffff, 0xffffff, 0.6);
+      xHemLight.position.set(48, 0, 0);
+      scene.add(xHemLight);
       function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement;
         const width = window.innerWidth;
@@ -548,4 +675,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.bar {
+  background: linear-gradient(
+    180deg,
+    rgb(166, 255, 164) 0%,
+    rgb(255, 236, 98) 31.94%,
+    rgb(255, 153, 49) 69.44%,
+    rgb(255, 50, 50) 100%
+  );
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
